cd2b138b7b2933997d4507e2f7de78abf86bd03a
Eigt <45220822+Eigt@users.noreply.github.com>
2020-09-12 16:05:44 -0400

    Initial upload
    Completed:
    - Background texture
    - Target texture
    - Start texture with child text
    - Cursor sprite with movement logic, including mouse sensitivity and deviation
    - On click, send position of cursor to target for comparison

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public GameObject m_cursor;
+    public bool m_isCursorVisible = false;
+    public float m_sensitivityX = 0.5f;
+    public float m_sensitivityY = 0.5f;
+    public float m_deviation = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = m_isCursorVisible;
+
+        transform.position = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Mouse X") * m_sensitivityX;
+        float moveY = Input.GetAxis("Mouse Y") * m_sensitivityY;
+
+        transform.Translate(moveX, moveY + moveX * m_deviation, 0);
+
+        // left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log($"X = {transform.position.x}, Y = {transform.position.y}");
+
+            // send position to target object
+            GameObject.FindGameObjectWithTag("Target").GetComponent<TargetController>().AddRelativeClick(transform.position);
+
+            // reset position of cursor
+            transform.position = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddRelativeClick(Vector3 a_clickPos)
+    {
+        Debug.Log($"X difference = {transform.position.x - a_clickPos.x}, Y difference = {transform.position.y - a_clickPos.y}");
+    }
+}
